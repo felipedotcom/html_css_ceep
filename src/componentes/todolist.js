31cd71fab1ef7e3b95377779be4fb79c40b10227
felipedotcom <felipe.rodolfo29@gmail.com>
2020-05-21 15:27:30 -0300

    remove tarefa e check na tarefa feito na mesma branch

--- src/componentes/todolist.js
+++ src/componentes/todolist.js
@@ -2,7 +2,7 @@ const novaTarefa = document.querySelector(".form-button");
 const ul = document.querySelector(".list")
 const input = document.querySelector('.form-input')
 
-const adicionaTarefa = (evento) => {
+const adicionaTarefar = (evento) => {
 
 	evento.preventDefault()
 
@@ -33,4 +33,27 @@ const adicionaTarefa = (evento) => {
 	
 };
 
-novaTarefa.addEventListener("click", adicionaTarefa)
+novaTarefa.addEventListener("click", adicionaTarefar)
+
+const deletarTarefa = (event) => { 
+	const tarefa = event.target
+
+	if(tarefa.classList[0] ==='material-icons'){ 
+		const tarefaList = tarefa.parentElement
+		tarefaList.remove()
+	}
+}
+
+ul.addEventListener('click', deletarTarefa)
+
+
+const marcarTarefaCompletada = (event) => { 
+	const tarefaCompleta = event.target
+
+	if(tarefaCompleta.classList[0] === 'check-button'){
+		const tarefaList = tarefaCompleta.parentElement
+		tarefaList.classList.toggle('done')
+	}
+}
+
+ul.addEventListener('click', marcarTarefaCompletada)
